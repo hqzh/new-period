3b4c435f7112f9128e54f47dc6550be98d182a0f
zhuhq <616928019@qq.com>
2018-12-30 19:12:03 +0800

    :lipstick: fs_open_fstat

--- fileSystem/writefile.js
+++ fileSystem/writefile.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 
-// 如果word.txt存在就会创建，flag的默认参数是w,
+// 如果word.txt存在就会创建，flag的默认参数是w,各类参数详见http://nodejs.cn/api/fs.html#fs_file_system_flags
 fs.writeFile('word.txt', "west word", { flag: 'a', encoding: "UTF-8" }, function (err) {
   if (err) {
     throw err;
