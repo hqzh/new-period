2912fa9e5e95850b27b63368be2a02a40bb89637
zhuhq <616928019@qq.com>
2019-01-05 21:17:44 +0800

    :whale: public

--- koa/static.js
+++ koa/static.js
@@ -3,3 +3,7 @@ const app = new Koa();
 const serve = require('Koa-static');
 app.use(serve(__dirname+'/html',{extensions:['html']}))
 app.listen(3000)
+
+// 通过下面部署到公网
+// npm install -g localtunnel
+// lt --subdomain chengwa --port 3000 /index
