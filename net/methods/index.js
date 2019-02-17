ce76a23e30165c9439568d52fd228e61bbf967e3
zhuhq <616928019@qq.com>
2019-02-17 20:28:16 +0800

    post

--- net/methods/index.js
+++ net/methods/index.js
@@ -1,19 +1,35 @@
 const http = require('http');
 const ejs = require('ejs');
 const url = require('url');
+const fs = require('fs');
 
-const server = http.createServer(function(req, res) {
+const server = http.createServer((req, res)=> {
   res.writeHead(200, { 'Content-Type': "text/html;charset='utf-8'" });
   const pathname = url.parse(req.url).pathname;
   if (pathname === '/login') {
-    ejs.renderFile('views/form.ejs', {}, function(err, data) {
+    ejs.renderFile('views/form.ejs', {}, (err, data) =>{
       res.end(data);
     });
   } else if (pathname === '/dologin') {
-    console.log(url.parse(req.url,true).query)
-    res.end('dologin');
+    // 获取get请求数据
+    // console.log(url.parse(req.url,true).query)
+    // 获取post请求数据
+    let postStr = '';
+    req.on('data', (chunk) =>{
+      postStr += chunk;
+    });
+    req.on('end', (err, chunk) =>{
+      fs.appendFile('login.txt', postStr + '\n', (err) =>{
+        if (err) {
+          console.log(err);
+          return;
+        }
+      });
+    });
+    console.log('写入成功');
+    res.end("<script>alert('登录成功')</script>");
   } else {
-    ejs.renderFile('views/home.ejs', {}, function(err, data) {
+    ejs.renderFile('views/home.ejs', {}, (err, data) =>{
       res.end(data);
     });
   }
