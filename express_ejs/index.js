1e28e5152725fd4d8eae6c4b717ff270a2482769
zhuhq <616928019@qq.com>
2019-03-27 21:58:07 +0800

    :hammer: static web server

---
+++ express_ejs/index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = new express();
+
+// app.engine('html', require('ejs').renderFile); //可以把ejs模板后缀改为html
+app.set('view engine', 'ejs'); //配置ejs模板引擎,ejs要写在views的文件夹里面,找文件默认会在这个文件夹里面找,后缀也可以省略
+// app.set('views', __dirname + '/statics'); //设置去根目录下的statics目录下面找views文件夹
+
+app.use(express.static(__dirname + '/public'));  //给public目录下面的文件提供静态web服务的中间件,可以写多个,  http://localhost:3001/css/style.css
+
+app.get('/', (req, res) => {
+  // res.render('index',{},(err,html)=>{
+  // 写了这个回调就要send或rerun
+  //   console.log(html)
+  //   res.send(html)
+  // })
+  res.render('index', { title: 'ejs模板' });
+});
+
+app.listen('3001', '127.0.0.1');
