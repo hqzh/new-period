891c4ad057ad667a9a72b736f60a5b838211da1b
zhuhq <616928019@qq.com>
2019-04-01 20:44:37 +0800

    :hammer: session

---
+++ express_ejs/express-session/index.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var session = require('express-session')
+
+var app = express();
+app.use(session({
+  secret: 'keyboard cat', //随便写的字符串参数,作为服务器端生成session的签名
+  name:'session_id',   // 保存在本地cookie的一个属性名,默认connect.sid, 可以不设置
+   resave: false,   //强制保存session,即使它并没有变化,默认为true,建议设置成false
+  saveUninitialized: true,  //强制将未初始化的session存储,就是未给session设置值的时候赋值,见下面默认设置true;
+  // cookie的所有参数都可以在这里配置
+  cookie: { 
+    // secure: true, //https可以访问cookie
+    maxAge:10000, //设置过期时间
+  }   
+}))
+
+app.get('/set', (req, res) => {
+  //  req.session;  //saveUninitialized: true,  //强制将未初始化的session存储,就是未给session设置值的时候赋值,见下面默认设置true;
+  req.session.userinfo ='hqzh' 
+  res.send('设置session成功');
+});
+
+
+app.get('/', (req, res) => {
+  console.log(req.session)
+  if (req.session.userinfo) {
+    res.send('hello,' + req.session.userinfo)
+  }else{
+    res.send('未登陆');
+  }
+  
+});
+
+app.listen(8080);
