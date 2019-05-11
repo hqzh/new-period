af647b7d443e56c303ee89bccddc09443edf344c
zhuhq <616928019@qq.com>
2019-05-11 19:43:57 +0800

    :heavy_plus_sign: 保存在服务器的session

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const render = require('koa-art-template');
 const bodyParser = require('koa-bodyparser');
 const static = require('koa-static');
 const path = require('path')
+const session = require('koa-session');
 const app = new koa();
 const router = new Router();
 app.use(bodyParser());
@@ -17,17 +18,35 @@ app.use(static(__dirname + '/static'))  //可配置多个静态托管
 //   }
 // }));
 
+
 render(app, {
   root: path.join(__dirname, 'views'),
   extname: '.art',
   debug: process.env.NODE_ENV !== 'production'
 });
 
+app.keys = ['some secret hurr'];  // cookie的签名
+ 
+const CONFIG = {
+  key: 'koa:sess', /** (string) cookie key (default is koa:sess) */
+  maxAge: 86400000,
+  autoCommit: true, /** (boolean) automatically commit headers (default true) */
+  overwrite: true, /** (boolean) can overwrite or not (default true) */
+  httpOnly: true, /** (boolean) httpOnly or not (default true) */
+  signed: true, /** (boolean) signed or not (default true) */
+  rolling: false, /** (boolean) Force a session identifier cookie to be set on every response. The expiration is reset to the original maxAge, resetting the expiration countdown. (default is false) */
+  renew: true, /** (boolean) renew session when session is nearly expired, so we can always keep user logged in. (default is false)*/
+};
+ 
+app.use(session(CONFIG, app));
+
 
 
 router.get('/', async (ctx, next) => {
   // await ctx.render('index')
   await ctx.render('index')
+  ctx.session.userinfo = 'root';
+  console.log(ctx.session.userinfo)
 });
 
 router.post('/doAdd', async (ctx, next) => {
