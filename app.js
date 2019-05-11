d58391078f773e2f8672b5423234ff8d87596ba3
zhuhq <616928019@qq.com>
2019-05-11 16:44:15 +0800

    :hammer: 原生

---
+++ app.js
@@ -0,0 +1,41 @@
+const koa = require('koa');
+const Router = require('koa-router');
+const views = require('koa-views');
+const common = require('./module/common.js')
+const app = new koa();
+const router = new Router();
+
+
+app.use(views(__dirname + '/views', {
+  map: {
+    html: 'ejs'
+  }
+}));
+
+router.get('/', async (ctx, next) => {
+  await ctx.render('index')
+});
+
+router.post('/doAdd', async (ctx, next) => {
+  const data = await common.getPostData(ctx)
+  console.log(data)
+});
+
+router.get('/news', (ctx, next) => {
+  ctx.body = 'news!';
+});
+
+router.get('/newscontent/:aid', (ctx, next) => {
+  ctx.body = ctx.params.aid;
+});
+
+app.use(async (ctx, next) => {
+  ctx.state.useinfo = 'hqzh'  //设置ejs全局属性
+  await next();
+  if (ctx.status === 404) {
+    ctx.body = '404'
+  }
+})
+
+app.use(router.routes())
+  .use(router.allowedMethods()).listen(8888);
